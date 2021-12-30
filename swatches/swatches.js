c8b67da9d3d50c25c76f9f992f9c04d531589cd6
Mohit Verma <mohitv891@gmail.com>
2021-12-30 19:27:39 +0530

    a

---
+++ swatches/swatches.js
@@ -0,0 +1,12 @@
+ jQuery(function() {
+     jQuery('.swatch :radio').change(function() {
+         var optionIndex = jQuery(this).closest('.swatch').attr('data-option-index');
+         var optionValue = jQuery(this).val();
+         jQuery(this)
+             .closest('form')
+             .find('.single-option-selector')
+             .eq(optionIndex)
+             .val(optionValue)
+             .trigger('change');
+     });
+ });
